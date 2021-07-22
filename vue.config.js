bfd4dfe2d33e7cedc2d5f67658b987495838db7e
ismuhuck <1562288394@qq.com>
2021-07-22 17:29:17 +0800

    更改端口

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   lintOnSave: false, // 是否开启esLint
   devServer: {
     host: '0.0.0.0',
-    port: 8082,
+    port: 4065,
     https: false, // 是否开启https
     hotOnly: true, // 是否开启热更新
     open: true // 是否自启动浏览器
