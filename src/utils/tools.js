e87725e77479eeb3320bb3655bcabb6fac37d7f0
ismuhuck <1562288394@qq.com>
2021-08-18 17:02:24 +0800

    数据类型判断工具函数

---
+++ src/utils/tools.js
@@ -0,0 +1,32 @@
+/**
+ * 判断数据类型
+ * @param {any} data 要判断数据类型的数据
+ * @param {string} type 目标类型
+ * @returns 是否是目标类型，当不传入指定type时返回相应的类型
+ */
+function isType(data, type) {
+  const typeObj = {
+    '[object String]': 'string',
+    '[object Number]': 'number',
+    '[object Boolean]': 'boolean',
+    '[object Null]': 'null',
+    '[object Undefined]': 'undefined',
+    '[object Object]': 'object',
+    '[object Array]': 'array',
+    '[object Function]': 'function',
+    '[object Date]': 'date', // Object.prototype.toString.call(new Date())
+    '[object RegExp]': 'regExp',
+    '[object Map]': 'map',
+    '[object Set]': 'set',
+    '[object HTMLDivElement]': 'dom', // document.querySelector('#app')
+    '[object WeakMap]': 'weakMap',
+    '[object Window]': 'window', // Object.prototype.toString.call(window)
+    '[object Error]': 'error', // new Error('1')
+    '[object Arguments]': 'arguments'
+  }
+  let name = Object.prototype.toString.call(data) // 借用Object.prototype.toString()获取数据类型
+  let typeName = typeObj[name] || '未知类型' // 匹配数据类型
+  return type ? typeName === type : typeName
+}
+
+export { isType }
